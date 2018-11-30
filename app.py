f27bc26078367b36863bf3e7f1c4fc7323b48204
suzieq <suziewizi@gmail.com>
2018-11-30 01:07:20 -0500

    Update the stage project[Part2]

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
-print(os.environ['APP_SETTINGS'])
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-#db=SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
 
-#from models import Result
+from models import Result
 
 @app.route('/')
 def hello():
