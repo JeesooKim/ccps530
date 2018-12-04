e00c2207fb438b21a772f806547ec62db87a8c8f
suzieq <suziewizi@gmail.com>
2018-12-04 03:30:28 -0500

    Update the landing page to load the data from db

--- app.py
+++ app.py
@@ -8,31 +8,78 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 
+engine =db.create_engine(os.environ['DATABASE_URL'])
+
 from models import Result
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    """ query data from the results table """
+    connection = None
+    try:
+        #https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91
+
+        connection = engine.connect()
+        metadata=db.MetaData()
+        results = db.Table('results', metadata, autoload=True, autoload_with=engine)
+
+        #print (results.columns.keys())
+        #print(repr(metadata.tables['results']))
+
+        #Equivalent to 'SELECT * FROM census'
+        query = db.select([results.columns.id, results.columns.url, results.columns.timestamp])    
+        #query="SELECT url, timestamp FROM results"
+        ResultProxy = connection.execute(query)
+        #print(ResultProxy)
+        ResultSet = ResultProxy.fetchall()
+        #ResultSet[:3]
+        
+        '''
+        users = db.Table('users', metadata, autoload=True, autoload_with=engine)
+        query2 = db.select([users])            
+        UserProxy = connection.execute(query2)        
+        UserSet = UserProxy.fetchall()
+        '''
+        connection.close()
+    #except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception, engine.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+
+    #return render_template('index.html', myCount=myCount)
+    return render_template('index.html', results = ResultSet, users=UserSet)
+    #return render_template('index.html')
+
+@app.route('/wordcount', methods=['GET', 'POST'])
+def wordcount():
     errors = []
     results = {}
     if request.method == "POST":
         # get url that the person has entered
         try:
             url = request.form['url']
+            #print(url)
             r = requests.get(url)
+            #print(r)
         except:
             errors.append(
                 "Unable to get URL. Please make sure it's valid and try again."
             )
-            return render_template('index.html', errors=errors)
+            return render_template('wordcount.html', errors=errors)
+            #return render_template('wordcount', errors=errors)
         if r:
             # text processing
-            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            #raw = BeautifulSoup(r.text).get_text()
+            raw = BeautifulSoup(r.text, "html.parser").get_text()
             nltk.data.path.append('./nltk_data/')  # set the path
             tokens = nltk.word_tokenize(raw)
             text = nltk.Text(tokens)
@@ -49,6 +96,7 @@ def index():
                 key=operator.itemgetter(1),
                 reverse=True
             )[:10]
+
             try:
                 result = Result(
                     url=url,
@@ -59,8 +107,8 @@ def index():
                 db.session.commit()
             except:
                 errors.append("Unable to add item to database.")
-    return render_template('index.html', errors=errors, results=results)
-
+    return render_template('wordcount.html', errors=errors, results=results)
+    #return render_template('wordcount', errors=errors, results=results)
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
-
+from sqlalchemy.sql import func
+import time
+from datetime import date
 
 class Result(db.Model):
     __tablename__ = 'results'
@@ -9,11 +11,18 @@ class Result(db.Model):
     url = db.Column(db.String())
     result_all = db.Column(JSON)
     result_no_stop_words = db.Column(JSON)
+    #timestamp = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    timestamp = db.Column(db.TIMESTAMP, default=func.now())
 
     def __init__(self, url, result_all, result_no_stop_words):
         self.url = url
         self.result_all = result_all
         self.result_no_stop_words = result_no_stop_words
+        self.timestamp = func.timezone('UTC', func.now())
+        #self.timestamp = date.fromtimestamp(time.time()) no time
+        #self.timestamp = func.timezone('UTC', func.current_timestamp())
+        #self.timestamp = func.timezone('UTC', func.localtimestamp())
+        #https://www.postgresql.org/docs/9.2/functions-datetime.html#FUNCTIONS-DATETIME-CURRENT
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
